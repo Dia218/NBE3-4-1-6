72d3f973999fc4113f2254d5364570211a9e6c68
TTaiJin <taeparis980110@gmail.com>
2025-01-09 17:15:29 +0900

    feat(back): getOrders()

--- src/main/java/org/team6/coffeebeanery/order/controller/SellerOrderController.java
+++ src/main/java/org/team6/coffeebeanery/order/controller/SellerOrderController.java
@@ -1,12 +1,23 @@
 package org.team6.coffeebeanery.order.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.team6.coffeebeanery.order.model.Order;
 import org.team6.coffeebeanery.order.service.SellerOrderService;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class SellerOrderController {
+
     private final SellerOrderService sellerOrderService;
 
+    @GetMapping("/admin/orders")
+    public ResponseEntity<List<Order>> getOrders() {
+        List<Order> orders = sellerOrderService.findAll();
+        return ResponseEntity.ok(orders);
+    }
 }
--- src/main/java/org/team6/coffeebeanery/order/service/SellerOrderService.java
+++ src/main/java/org/team6/coffeebeanery/order/service/SellerOrderService.java
@@ -1,12 +1,21 @@
 package org.team6.coffeebeanery.order.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.team6.coffeebeanery.order.model.Order;
 import org.team6.coffeebeanery.order.repository.OrderRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class SellerOrderService {
     private final OrderRepository orderRepository;
-    
+
+    public List<Order> findAll() {
+        // 주문 일자 최신순으로 정렬
+        Sort sort = Sort.by(Sort.Order.desc("orderCreatedAt"));
+        return orderRepository.findAll(sort);
+    }
 }
