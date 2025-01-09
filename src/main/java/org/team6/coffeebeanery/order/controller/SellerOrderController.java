042354fcbe00d719b0295025b0807f3b53f76060
TTaiJin <taeparis980110@gmail.com>
2025-01-09 17:03:16 +0900

    refactor(back): Order Entity 상세 주소 3개 Address로 변경

--- src/main/java/org/team6/coffeebeanery/order/controller/SellerOrderController.java
+++ src/main/java/org/team6/coffeebeanery/order/controller/SellerOrderController.java
@@ -1,12 +1,12 @@
 package org.team6.coffeebeanery.order.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.team6.coffeebeanery.order.service.SellerOrderService;
 import org.springframework.web.bind.annotation.RestController;
+import org.team6.coffeebeanery.order.service.SellerOrderService;
 
 @RequiredArgsConstructor
 @RestController
 public class SellerOrderController {
     private final SellerOrderService sellerOrderService;
-    
+
 }
--- src/main/java/org/team6/coffeebeanery/order/model/Order.java
+++ src/main/java/org/team6/coffeebeanery/order/model/Order.java
@@ -1,14 +1,18 @@
 package org.team6.coffeebeanery.order.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
 import org.team6.coffeebeanery.common.constant.OrderStatus;
+import org.team6.coffeebeanery.common.model.Address;
 import org.team6.coffeebeanery.delivery.model.Delivery;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @Entity
@@ -17,19 +21,19 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer orderId; //주문 ID
     
-    @Column(length = 254)
+    @Column(length = 254, nullable = false)
     private String customerEmail; //고객 이메일
-    
-    private String baseAddress; //기본 주소
-    
-    private String detailAddress; //상세 주소
-    
-    private String zipCode; //우편 주소
-    
+
+    @Embedded
+    private Address address; // 상세 주소 합친거
+
+    @CreatedDate
     private LocalDateTime orderCreatedAt; //주문 날짜 및 시간
-    
+
+    @Column(nullable = false)
     private Double totalPrice; //총 주문 금액
-    
+
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus; //주문 상태
     
