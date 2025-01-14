38330c06d3af04adb074060cba7c8e13cb0011cd
TTaiJin <taeparis980110@gmail.com>
2025-01-14 15:08:02 +0900

    ready to PR

--- src/main/java/org/team6/coffeebeanery/order/repository/OrderRepository.java
+++ src/main/java/org/team6/coffeebeanery/order/repository/OrderRepository.java
@@ -1,7 +1,10 @@
 package org.team6.coffeebeanery.order.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.team6.coffeebeanery.order.model.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Page<Order> findAllByCustomerEmail(String customerEmail, Pageable pageable);
 }
