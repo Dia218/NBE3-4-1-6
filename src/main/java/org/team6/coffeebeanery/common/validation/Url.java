be822b3945b863d22b17823624209f273e043041
dia218 <sang218@kyonggi.ac.kr>
2025-01-16 15:09:46 +0900

    fix(Url): Fix Url Annotation
    - @Contraint need groups param. must add it.

--- src/main/java/org/team6/coffeebeanery/common/validation/Url.java
+++ src/main/java/org/team6/coffeebeanery/common/validation/Url.java
@@ -1,6 +1,7 @@
 package org.team6.coffeebeanery.common.validation;
 
 import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -8,10 +9,15 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Constraint(validatedBy = UrlValidator.class)
-@Target({ElementType.FIELD, ElementType.PARAMETER })
+@Target({ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Url {
     
     String message() default "유효한 URL이 아닙니다.";
+    
+    // groups 파라미터 추가
+    Class<?>[] groups() default {};  // groups 속성 추가
+    
+    Class<? extends Payload>[] payload() default {};
     
 }
