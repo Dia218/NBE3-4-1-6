f6e40878a8bae86ba51441c9a96c9c6146fa240e
c-hano <wdq0304@naver.com>
2025-01-20 14:04:36 +0900

    refactor: add OrderDTO in Delivery to pull front

--- src/main/java/org/team6/coffeebeanery/delivery/dto/DeliveryDTO.java
+++ src/main/java/org/team6/coffeebeanery/delivery/dto/DeliveryDTO.java
@@ -3,6 +3,8 @@ package org.team6.coffeebeanery.delivery.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.team6.coffeebeanery.order.dto.OrderDTO;
+import org.team6.coffeebeanery.order.model.Order;
 
 @Getter
 @Setter
@@ -12,4 +14,6 @@ public class DeliveryDTO {
     private String deliveryNumber; // 운송장 번호
     private String deliveryCompany; // 택배사
     private Long orderId; // 주문 ID (추가)
+    private OrderDTO orderDTO;
+    private Order order;
 }
