de7028c330bf829bdcc1c70b1829c72917e82306
LeeKyoWon <kskw20@naver.com>
2025-01-20 15:24:50 +0900

    feat(OrderRepository): add batch query functions

--- src/main/java/org/team6/coffeebeanery/order/repository/OrderRepository.java
+++ src/main/java/org/team6/coffeebeanery/order/repository/OrderRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.team6.coffeebeanery.common.constant.OrderStatus;
 import org.team6.coffeebeanery.order.model.Order;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -13,5 +15,14 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     List<Order> findAllByCustomerEmail(String email);
     Page<Order> findAllByCustomerEmail(String customerEmail, Pageable pageable);
+    List<Order> findByOrderStatus(OrderStatus orderStatus);
 
+    List<Order> findByOrderCreatedAtBetween(
+            LocalDateTime startDateTime,
+            LocalDateTime endDateTime);
+
+    List<Order> findByOrderCreatedAtBetweenAndOrderStatus(
+            LocalDateTime startDateTime,
+            LocalDateTime endDateTime,
+            OrderStatus orderStatus);
 }
